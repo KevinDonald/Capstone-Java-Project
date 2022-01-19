a6fde66a6d38c37b92c582d03f09c08db959a1a5
Kevin Donald <kevin.d.donald-1@ou.edu>
2022-01-19 07:49:51 +0000

    Reads text file, parses by comma

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,37 @@
 package com.mycompany.app;
 
+import java.nio.file.*;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
+/*
+ * Used https://stackoverflow.com/questions/326390/how-do-i-create-a-java-string-from-the-contents-of-a-file 
+ * code for reading in String easily
+ *
+ * https://stackoverflow.com/questions/26268132/all-inclusive-charset-to-avoid-java-nio-charset-malformedinputexception-input
+ * for error checking
+ *
+ * And a lot of Javadocs and one quick search on array length
+*/
+
 public class App 
 {
     public static void main( String[] args )
     {
-        String test = "baby,toddler,child,teen,adult,elderly";
-	String[] items = test.split(",");
-
-	for (int i = 0; i < items.length; ++i)
+	try
 	{
-		System.out.println(items[i]);
+		String csvLetters = Files.readString(Paths.get(args[0]), StandardCharsets.UTF_8);
+		String[] items = csvLetters.split(",");
+
+		for (int i = 0; i < items.length; ++i)
+		{
+			System.out.println(items[i]);
+		}
+	}
+	catch (IOException e)
+	{
+		System.out.println("Please specify file path in args: e.g., java App.java \"nameOfFile.txt\".");
+		System.out.println(e);
 	}
     }
 }
